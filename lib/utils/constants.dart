da44b76320e06be93cb3a088954c59714db68484
nikhil888-teach <gonikhil8@gmail.com>
2023-02-01 21:06:55 +0530

    i add my files

---
+++ lib/utils/constants.dart
@@ -0,0 +1,50 @@
+class Constants {
+  static const APP_NAME = "E-Commerce";
+
+  //MAIN_PAGE
+  static const HOME = "Home";
+  static const shop = "Shop";
+  static const bag = "Bag";
+  static const Profile = "Profile";
+
+  //LOGIN_PAGE
+  static const LOGIN = "Login";
+  static const EMAIL = "Email";
+  static const PASSWORD = "Password";
+  static const FORGOT_PASSWORD = "Forgot your password";
+  static const OR_SIGN_WITH_SOCIAL = "Or sign up with Social account";
+
+  //SINGUP_PAGE
+  static const SIGNUP = "Sign up";
+  static const NAME = "Name";
+  static const ALREADY_ACCOUNT = "Already have an account?";
+
+  //HOME_PAGE
+  static const FASHION = "Fashion";
+  static const SALE = "Sale";
+  static const CHECK = "Check";
+  static const NEW = "New";
+  static const you_never_before = "You've never seen it before";
+
+  //FAVORITE_PAGE
+  static const Favorites = "Favorites";
+  static const Price = "Price";
+
+  //BAG_PAGE
+  static const My_Bag = "My Bag";
+  static const ADD_FAVORITE = "Add to favorities";
+  static const delete_list = "Delete from the list";
+  static const color = "Color: ";
+  static const size = "Size: ";
+  static const total_amount = "Total Amount:";
+  static const check_out = "CHECK OUT";
+
+  //SHOPE_PAGE
+  static const Categories = "Categories";
+  static const women = "Women";
+  static const Men = "Men";
+  static const kides = "Kids";
+
+  //PROFILE_PAGE
+  static const my_profile = "My profile";
+}
